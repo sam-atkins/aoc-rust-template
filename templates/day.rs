7e1c92203104497c331a83f139df9db403a1780a
Sam Atkins <butlers.tunic.04@icloud.com>
2023-12-20 08:32:05 +0000

    refactor template reading input files

--- templates/day.rs
+++ templates/day.rs
@@ -1,14 +1,8 @@
-use aoc_rust_2023::reader;
-
-const DAY: u8 = {{ day }};
-
 fn main() {
-    let example_path = format!("data/day_{:02}/EXAMPLE_02.txt", DAY);
-    let example_input = reader::file_to_string(&example_path);
+    let example_input = include_str!("../../data/day_{{ day }}/EXAMPLE_01.txt");
     println!("Example: {:?}", process(&example_input));
 
-    // let input_path = format!("data/day_{:02}/INPUT.txt", DAY);
-    // let input = reader::file_to_string(&input_path);
+    // let input = include_str!("../../data/day_{{ day }}/INPUT.txt");
     // println!("Input {:?}", process(&input));
 }
 
@@ -26,8 +20,7 @@ mod tests_day_{{ day }}_part_{{ part }} {
     #[test]
     #[ignore]
     fn test_process_example() {
-        let example_path = format!("data/day_{:02}/EXAMPLE_01.txt", DAY);
-        let input = reader::file_to_string(&example_path);
+        let input = include_str!("../../data/day_{{ day }}/EXAMPLE_01.txt");
         let result = process(&input);
         assert_eq!(result, None);
     }
@@ -35,8 +28,7 @@ mod tests_day_{{ day }}_part_{{ part }} {
     #[test]
     #[ignore]
     fn test_process_input() {
-        let input_path = format!("data/day_{:02}/INPUT.txt", DAY);
-        let input = reader::file_to_string(&input_path);
+        let input = include_str!("../../data/day_{{ day }}/INPUT.txt");
         let result = process(&input);
         assert_eq!(result, None);
     }
